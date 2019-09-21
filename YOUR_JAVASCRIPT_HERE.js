041384c75856d3bdbdee1c73d1ec03586f92b3a7
azenui25 <asanga.nicholas@yahoo.com>
2019-09-21 07:13:19 +0200

    section one finished

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,40 @@
+// create hero object
+
+const hero = {
+        name: 'Asanga',
+        heroic: true,
+        inventory: [],
+        health: 10,
+        weapon: { type: 'AK47', damage: 2 }
+    }
+    // console.log(hero)
+    // console.log(hero.weapon.damage)
+
+// game logic functions
+
+function rest(hero) {
+    hero.health = 10
+    return hero
+
+    if (hero.health = 10) {
+        alert('this nigger is healthy')
+        return true
+    }
+
+
+
+
+}
+
+
+
+
+function pickUpItem() {
+
+
+}
+
+function equipWeapon(hero) {
+
+
+}
